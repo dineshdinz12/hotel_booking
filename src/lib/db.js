645e6ab45b0f70cd821b646701fd61d99f043270
Prasanth P <155311223+prasanth45bit@users.noreply.github.com>
2024-12-23 11:23:00 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,8 +4,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: 'localhost', // Wrap in quotes
   user: 'root',      // Wrap in quotes
-  password: 'PMTP@4273', // Ensure it's the correct password
-  database: 'hotelbooking', // Wrap in quotes
+  password: 'Password', // Ensure it's the correct password
+  database: 'hotelbookin', // Wrap in quotes
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
